9684bb838a7db8d5ae02569f9f0a2853fc7020a9
Juhan Nguyen <dungnq49@fpt.com>
2025-10-18 22:25:08 +0700

    Init commit

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 """LangChain agent application."""
 
-import getpass
-import os
+from langchain.agents import create_agent
 
-if not os.environ.get("GOOGLE_API_KEY"):
-  os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
 
-from langchain.chat_models import init_chat_model
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
 
-model = init_chat_model("gemini-2.5-pro", model_provider="google_genai")
 
-model.invoke("Hello, world!")
+agent = create_agent(
+    model="anthropic:claude-sonnet-4-5",
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
+
+# Run the agent
+agent.invoke({"messages": [{"role": "user", "content": "what is the weather in sf"}]})
